979306a7f78eb6d1a4f0df64f7d548bb8ede7dfa
Shawn Kim <sdf1342@gmail.com>
2018-09-19 08:20:44 -0700

    List View, Button, Date & Time added.

---
+++ Technical Test/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClick()
+    {
+        GameObject button = EventSystem.current.currentSelectedGameObject;
+
+        if (button != null)
+        {
+            Debug.Log(button + "Clickced");
+            button.GetComponent<Image>().color = new Color32(11, 121, 222, 255);
+        }
+            
+
+    }
+
+}
---
+++ Technical Test/Assets/Scripts/Date.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Date : MonoBehaviour
+{
+    private System.DateTime curDate;
+
+    [SerializeField]
+    public Text date;
+    [SerializeField]
+    public Text time;
+
+    void Start()
+    {
+        UpdateGUIDateTime(System.DateTime.Now);
+        StartCoroutine(OnPerMin());
+
+    }
+
+    /// <summary>
+    /// Coroutine function that check the time call update every 0.5 sec
+    /// </summary>
+    /// <returns>The per minimum.</returns>
+    private IEnumerator OnPerMin()
+    {
+        while(true)
+        {
+            // update time
+            curDate = System.DateTime.Now;
+
+
+            // if one minute is passed
+            if((curDate.Second % 60) == 0)
+            {
+                Debug.Log("one minute passed: " + curDate);
+
+                UpdateGUIDateTime(curDate);
+            }
+
+
+            // every 0.5 second
+            yield return new WaitForSecondsRealtime(0.5f);
+        }
+    }
+
+    /// <summary>
+    /// Updates the GUI's Date and time.
+    /// </summary>
+    /// <param name="newDateTime">New Date and Time.</param>
+    private void UpdateGUIDateTime(System.DateTime newDateTime)
+    {
+        // safety
+        if (date != null && time != null)
+        {
+            date.text = newDateTime.ToString("dd MM yyyy");
+            time.text = newDateTime.ToString("HH:mm");
+        }
+        
+    }
+}
