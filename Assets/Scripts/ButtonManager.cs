a6e73e33b22b04ea6ae4eaa8c7ff9125e4fc3f64
Shawn Kim <sdf1342@gmail.com>
2018-09-22 03:50:33 -0700

    Add button highlighting when clicked, but not yet for disabling highlighting on hover.

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Color newColor = new Color32(0, 0, 0, 255);
+
+    public void OnClick()
+    {
+        Button button = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+
+
+        Debug.Log(button + "Clickced");
+
+        if (button != null)
+        {
+            ColorBlock colors = button.colors;
+            colors.highlightedColor = new Color32(50, 100, 200, 255);
+            button.colors = colors;
+        }
+    }
+
+}
