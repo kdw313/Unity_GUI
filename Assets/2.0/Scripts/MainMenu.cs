391abd005daf9be3a4bf85e02ef944d940fde1ff
Shawn Kim <sdf1342@gmail.com>
2018-10-07 10:06:32 -0700

    Main menu animation & applying

---
+++ Assets/2.0/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Main menu.
+/// </summary>
+public class MainMenu : MonoBehaviour {
+
+    /// <summary>
+    /// Plaies the game.
+    /// </summary>
+    public void PlayGame()
+    {
+        // TODO: Load next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>
+    /// Quits the game.
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
